b3f9ad4c3aa84b5d74e25d47b9bbad85fb6a60a9
fcogallegos <fgallegosgo@gmail.com>
2022-01-03 12:39:17 -0700

    introduction to pipes in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+
+  name: string = 'francisco gallegos';
+  value: number = 1024;
+  obj: object = {
+    name: 'Liz'
+  }
+
+  showName() {
+    console.log(this.name);
+    console.log(this.value);
+    console.log(this.obj);
+  }
 }
