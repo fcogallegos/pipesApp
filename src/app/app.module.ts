594cd458fba57a1d4961fb8ae24eb04c54e32a21
fcogallegos <fgallegosgo@gmail.com>
2022-01-04 10:40:31 -0700

    cards and buttons with icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 
 //PrimeNg
 import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import {ButtonModule} from 'primeng/button';
   ],
   imports: [
     BrowserModule,
-    ButtonModule
+    ButtonModule,
+    CardModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
