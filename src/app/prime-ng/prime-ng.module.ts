8290b24cfed078f02c1b552e2befce96905e3161
fcogallegos <fgallegosgo@gmail.com>
2022-01-04 12:49:16 -0700

    PrimeNg - MenuBar

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 //PrimeNg
 import {ButtonModule} from 'primeng/button';
 import {CardModule} from 'primeng/card';
+import {MenubarModule} from 'primeng/menubar';
 
 
 
@@ -12,7 +13,9 @@ import {CardModule} from 'primeng/card';
   exports: [
     CommonModule,
     ButtonModule,
-    CardModule
+    CardModule,
+    MenubarModule,
+  
   ]
 })
 export class PrimeNgModule { }
