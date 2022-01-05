b32722c38543829a10a9fff4b637f53f80166f73
fcogallegos <fgallegosgo@gmail.com>
2022-01-05 10:18:13 -0700

    routes of our app

---
+++ src/app/app-router.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { BasicsComponent } from './sales/pages/basics/basics.component';
+import { NumbersComponent } from './sales/pages/numbers/numbers.component';
+import { NotCommonComponent } from './sales/pages/not-common/not-common.component';
+import { OrderComponent } from './sales/pages/order/order.component';
+
+
+const routes: Routes = [
+
+  {
+    path: '',
+    component: BasicsComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'numbers',
+    component: NumbersComponent
+  },
+  {
+    path: 'not-common',
+    component: NotCommonComponent
+  },
+  {
+    path: 'basics',
+    component: BasicsComponent
+  },
+  {
+    path: 'order',
+    component: OrderComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+
+];
+
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot( routes )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRouterModule { }
--- src/app/sales/sales.module.ts
+++ src/app/sales/sales.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NumbersComponent } from './pages/numbers/numbers.component';
+import { NotCommonComponent } from './pages/not-common/not-common.component';
+import { OrderComponent } from './pages/order/order.component';
+import { BasicsComponent } from './pages/basics/basics.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NumbersComponent,
+    NotCommonComponent,
+    OrderComponent,
+    BasicsComponent
+  ],
+  exports: [
+    NumbersComponent,
+    NotCommonComponent,
+    OrderComponent,
+    BasicsComponent
+  ],
   imports: [
     CommonModule
   ]
